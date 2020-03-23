3189f265e01965adfbf5b76c5c42fd391645de2c
Simone Bartolini <simone@MacBook-Pro-di-Simone-Bartolini.local>
2020-03-23 19:28:28 +0100

    changed message period to 10 minutes

--- things/Environmental_station.py
+++ things/Environmental_station.py
@@ -10,46 +10,46 @@ class EnvironmentalStation(Thread):
     windDirection = 0
     windIntensity = 0
     rainHeight = 0
-    
+
     def __init__(self, name):
         Thread.__init__(self)
         self.name = name
         self.MQTTClient = AWSIoTMQTTClient(name)
-    
+
     def updateSensors(self):
         self.temperature = random.randint(-50, 51)
         self.humidity = random.randint(0, 101)
         self.windDirection = random.randint(0, 361)
         self.windIntensity = random.randint(0, 101)
         self.rainHeight = random.randint(0, 51)
-    
+
     def publish(self):
-        return self.MQTTClient.publish("stations/" + self.name, 
+        return self.MQTTClient.publish("stations/" + self.name,
                                        '{"temperature": "' + str(self.temperature) + '",' +
                                         '"humidity": "' + str(self.humidity) + '",' +
                                         '"windDirection": "' + str(self.windDirection) + '",' +
                                         '"windIntensity": "' + str(self.windIntensity) + '",' +
                                         '"rainHeight": "' + str(self.rainHeight) + '"}', 0)
-        
+
     def configureMQTT(self):
         self.MQTTClient.configureEndpoint("a29wnmzjyb35x8-ats.iot.us-east-1.amazonaws.com", 8883)
-        self.MQTTClient.configureCredentials("AmazonRootCA1.pem.crt", "e0a2ae42f8-private.pem.key", 
+        self.MQTTClient.configureCredentials("AmazonRootCA1.pem.crt", "e0a2ae42f8-private.pem.key",
                                              "e0a2ae42f8-certificate.pem.crt")
         self.MQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
         self.MQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
         self.MQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
         self.MQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-    
+
     def connect(self):
         return self.MQTTClient.connect()
-        
+
     def disconnect(self):
         return self.MQTTClient.disconnect()
-    
+
     def run(self):
         self.configureMQTT()
         while start:
-            time.sleep(5)
+            time.sleep(600)
             try:
                 self.connect()
             except:
@@ -68,14 +68,14 @@ while True:
         continue
     break
 
-stationList = []   
+stationList = []
 for i in range(0, numStation):
     stationList.append(EnvironmentalStation("environmentalStation" + str(i)))
-    
+
 start = True
 for station in stationList:
     station.start()
-    
+
 while True:
     if input("Enter 'stop' to stop the stations\n")=='stop':
         start = False
