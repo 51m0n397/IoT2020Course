012fd345e9378909047ca33aa9f8b153df192824
Simone Bartolini <simone@MacBook-Pro-di-Simone-Bartolini.local>
2020-04-16 13:49:52 +0200

    changes to the comments

--- lora-MQTT-bridge/lora-MQTT-bridge.py
+++ lora-MQTT-bridge/lora-MQTT-bridge.py
@@ -6,14 +6,17 @@ ttnClient = mqtt.Client()
 awsClient = AWSIoTMQTTClient("TTNbridge")
 
 
-
+# The callback for when the TTN MQTT client connects.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("+/devices/+/up")
 
-# The callback for when a PUBLISH message is received from the server.
+# The callback for when a message is received from the TTN broker.
+# When a message from TTN is received the "payload_fields" field is estracted
+# and published to the AWS MQTT broker on the topic "stations/" + the id
+# of the device that sent the message.
 def on_message(client, userdata, msg):
     parsed_json = (json.loads(msg.payload.decode("utf-8")))
     print("stations/"+parsed_json["dev_id"],
@@ -22,7 +25,7 @@ def on_message(client, userdata, msg):
                       json.dumps(parsed_json["payload_fields"]), 0)
 
 
-# AWS client configuration
+# AWS client configuration.
 
 # Folder containing the certificates and the private key for
 # authenticating with the AWS mqtt broker.
@@ -32,7 +35,7 @@ certFolder = "../environmental_station/"
 # MAKE SHURE to insert the correct name for your endpoit,
 # your certificates and the key.
 awsClient.configureEndpoint("a29wnmzjyb35x8-ats.iot.us-east-1.amazonaws.com",
-                             8883)
+                            8883)
 awsClient.configureCredentials(certFolder + "AmazonRootCA1.pem.crt",
                                certFolder + "e0a2ae42f8-private.pem.key",
                                certFolder + "e0a2ae42f8-certificate.pem.crt")
@@ -44,24 +47,29 @@ awsClient.configureConnectDisconnectTimeout(10)  # 10 sec
 awsClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 
-
-# TTN client configuration
+# TTN client configuration.
 ttnClient.on_connect = on_connect
 ttnClient.on_message = on_message
 with open("key.txt", "r") as key:
     ttnClient.username_pw_set(key.readline().strip(), key.readline().strip())
 ttnClient.tls_set(ca_certs="mqtt-ca.pem.crt")
 
+
 # Connecting the clients.
 ttnClient.connect("eu.thethings.network", 8883, 60)
 awsClient.connect()
 
+
 ttnClient.loop_start()
 
+# Main cicle.
 while True:
     if input("Enter 'stop' to stop the bridge\n") == 'stop':
         break
 
 ttnClient.loop_stop()
+
+
+# Disconnectin the clients.
 ttnClient.disconnect()
 awsClient.disconnect()
