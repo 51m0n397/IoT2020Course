674046b183929de69d6ce27772d804fc4f068df1
Simone Bartolini <simone@MacBook-Pro-di-Simone-Bartolini.local>
2020-05-13 14:37:10 +0200

    bug fix

--- crowd-sensing/edge-based/smartphone-webapp/bundle.js
+++ crowd-sensing/edge-based/smartphone-webapp/bundle.js
@@ -27,6 +27,22 @@
   // Configuration of the AWS SDK.
   //
 
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   //The id of the MQTT client.
   var clientId = getCookie("clientId");
   if (clientId == "") {
