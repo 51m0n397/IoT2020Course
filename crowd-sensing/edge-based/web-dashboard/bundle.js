d5b549e1cefaa33f55e6ec70c3f12b560c560423
Simone Bartolini <simone@MacBook-Pro-di-Simone-Bartolini.local>
2020-05-19 17:51:45 +0200

    little improvements

--- crowd-sensing/edge-based/web-dashboard/bundle.js
+++ crowd-sensing/edge-based/web-dashboard/bundle.js
@@ -1,31 +1,27 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
-  /*
-   * The awsConfiguration object is used to store the credentials
-   * to connect to AWS service.
-   * MAKE SHURE to insert the correct name for your endpoint,
-   * the correct Cognito PoolID and the correct AWS region.
-   */
-  var awsConfiguration = {
-    poolId: 'us-east-1:1535c67e-dee5-4f0f-9bbe-06f46c1dcfbb',
-    host: "a29wnmzjyb35x8-ats.iot.us-east-1.amazonaws.com",
-    region: 'us-east-1'
-  };
-  module.exports = awsConfiguration;
-
-}, {}], 2: [function(require, module, exports) {
-
-  //Loading the AWS SDK and the configuration objects.
+  //Loading libraries
   var AWS = require('aws-sdk');
   var AWSIoTData = require('aws-iot-device-sdk');
-  var AWSConfiguration = require('./aws-configuration.js');
-  console.log('Loaded AWS SDK for JavaScript and AWS IoT SDK for Node.js');
 
 
 
   //
   // Configuration of the AWS SDK.
   //
+
+
+  /*
+   * The awsConfiguration object is used to store the credentials
+   * to connect to AWS service.
+   * MAKE SHURE to insert the correct name for your endpoint,
+   * the correct Cognito PoolID and the correct AWS region.
+   */
+  var AWSConfiguration = {
+    poolId: 'us-east-1:1535c67e-dee5-4f0f-9bbe-06f46c1dcfbb',
+    host: "a29wnmzjyb35x8-ats.iot.us-east-1.amazonaws.com",
+    region: 'us-east-1'
+  };
 
   //The id of the MQTT client.
   var clientId = 'EdgeHARDashboard-' + (Math.floor((Math.random() * 100000) + 1));
@@ -90,14 +86,23 @@
       document.getElementById("past-values-link").className = "inactive";
       document.getElementById('current-values-div').style.display = 'block';
       document.getElementById('past-values-div').style.display = 'none';
-      updateInfo();
+      if (currentDevice != "") updateCurrentValues();
     } else {
       document.getElementById("current-values-link").className = "inactive";
       document.getElementById("past-values-link").className = "active";
       document.getElementById('current-values-div').style.display = 'none';
       document.getElementById('past-values-div').style.display = 'block';
-      refreshChart();
+      if (currentDevice != "") refreshChart();
     }
+  }
+
+  //Function for changing the currently displayed device.
+  window.changeDevice = function() {
+    currentDevice = document.getElementById("device-select").value;
+    if(document.getElementById('current-values-div').style.display=="block")
+      updateCurrentValues();
+    else refreshChart();
+    document.getElementById('refresh-chart').disabled = false;
   }
 
   //
@@ -109,6 +114,9 @@
 
   //Variable storing the current values of the devices.
   var devicesValues = {};
+
+  //Variable storing the name of the station we are currently displaying.
+  var currentDevice = "";
 
   //Parameters for the scan of the database.
   var scanParams = {
@@ -146,6 +154,7 @@
           },
           KeyConditionExpression: "#id = :deivce",
           TableName: 'EdgeHAR',
+          Limit: 1,
           "ScanIndexForward": false
         };
 
@@ -175,19 +184,18 @@
   //The topic where the environmental stations publish the sensors data.
   var deviceTopic = 'EdgeComputing/+';
 
-  //Variable storing the name of the station we are currently displaying.
-  var currentDevice = "";
+
 
   //Connect handler: once the MQTT client has successfully connected
-  //to the MQTT server it subscribes to the stationTopic
-  window.mqttClientConnectHandler = function() {
+  //to the MQTT server it subscribes to the deviceTopic
+function mqttClientConnectHandler() {
     console.log('connected to MQTT server');
     mqttClient.subscribe(deviceTopic);
     console.log("subscribed to", deviceTopic);
   };
 
   //Function for updating the div containing the current status of the device.
-  window.updateInfo = function() {
+  function updateCurrentValues() {
     var status = document.getElementById("device-status");
     status.innerHTML = devicesValues[currentDevice];
   }
@@ -195,7 +203,7 @@
   //Message handler: upon receiving a message if it's relative to a new device
   //it adds it to the selection menu then it saves it's values in the variable
   //devicesValues and finally updates the div.
-  window.mqttClientMessageHandler = function(topic, payload) {
+  function mqttClientMessageHandler(topic, payload) {
     console.log('message: ' + topic + ':' + payload.toString());
     if (devicesValues[topic.slice(14)] == undefined) {
       devicesList.push(topic.slice(14));
@@ -204,23 +212,15 @@
     }
     devicesValues[topic.slice(14)] = JSON.parse(payload.toString()).status;
     if (currentDevice != ""){
-      if(document.getElementById('current-values-div').style.display=="block")
-        updateInfo();
-      else refreshChart();
+      updateCurrentValues();
     }
   };
 
-  //Function for changing the currently displayed device.
-  window.changeDevice = function() {
-    currentDevice = document.getElementById("device-select").value;
-    if(document.getElementById('current-values-div').style.display=="block")
-      updateInfo();
-    else refreshChart();
-  }
-
   //Installing the connect and message handlers.
-  mqttClient.on('connect', window.mqttClientConnectHandler);
-  mqttClient.on('message', window.mqttClientMessageHandler);
+  mqttClient.on('connect', mqttClientConnectHandler);
+  mqttClient.on('message', mqttClientMessageHandler);
+
+
 
 
 
@@ -229,13 +229,13 @@
   //
 
   //Function that returns the timestamp of one hour ago.
-  window.lastHour = function() {
+  function lastHour() {
     var d = new Date();
     d.setHours(d.getHours() - 1);
     return d.getTime();
   }
 
-  //Queries the database for the sensor data and draws the charts.
+  //Function for updating the chart displaying the data from the past hour.
   window.refreshChart = function() {
 
     //Parameters of the query.
@@ -276,12 +276,12 @@
         });
 
         //Draws the chart.
-        let chart = new Chart(document.getElementById('chart').getContext('2d'), {
+        var chart = new Chart(document.getElementById('chart').getContext('2d'), {
           type: "line",
           data: {
             labels: time,
             datasets: [{
-              label: currentDevice,
+              label: "Status",
               data: dataset,
               fill: false,
               borderColor: "rgb(255, 0, 0)",
@@ -326,5 +326,4 @@
 
   }
 
-
-},{"./aws-configuration.js":1,"aws-iot-device-sdk":"aws-iot-device-sdk","aws-sdk":"aws-sdk"}]},{},[2]);
+},{"aws-iot-device-sdk":undefined,"aws-sdk":undefined}]},{},[1]);
